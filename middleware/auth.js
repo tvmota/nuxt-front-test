fb8401f8f6fd510899e629c9fe71c78a2a8bb3b9
tiago.vieira.mota@gmail.com <t.tiago.mota@uello.com.br>
2024-02-10 18:56:34 -0300

    update head repo

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,13 @@
 import { useStorage } from '@vueuse/core';
 
-/*eslint-disable*/
+// eslint-disable-next-line no-undef
 export default defineNuxtRouteMiddleware(() => {
   const timestamp = useStorage('timestamp');
   const timeout = new Date(timestamp.value) < new Date();
 
   if (timeout) {
     process.client && console.log('Timeout', new Date(timestamp.value));
+    // eslint-disable-next-line no-undef
     return navigateTo('/');
   }
 });
